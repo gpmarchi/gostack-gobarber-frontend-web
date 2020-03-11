9013a390d37c207d408dc45bcca1e145612498b1
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2020-03-11 16:07:58 -0300

    app global stylying

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Router } from 'react-router-dom';
 
 import './config/ReactotronConfig';
+import GlobalStyle from './styles/global';
 
 import Routes from './routes';
 import history from './services/history';
@@ -9,6 +10,7 @@ import history from './services/history';
 function App() {
   return (
     <Router history={history}>
+      <GlobalStyle />
       <Routes />
     </Router>
   );
