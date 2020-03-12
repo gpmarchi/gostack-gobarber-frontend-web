d903195776e971d428a28f4195eb30c9d430f27f
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2020-03-12 16:57:48 -0300

    persist token and user details to local storage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import './config/ReactotronConfig';
 
@@ -8,15 +9,17 @@ import GlobalStyle from './styles/global';
 
 import Routes from './routes';
 import history from './services/history';
-import store from './store';
+import { store, persistor } from './store';
 
 function App() {
   return (
     <Provider store={store}>
-      <Router history={history}>
-        <GlobalStyle />
-        <Routes />
-      </Router>
+      <PersistGate persistor={persistor}>
+        <Router history={history}>
+          <GlobalStyle />
+          <Routes />
+        </Router>
+      </PersistGate>
     </Provider>
   );
 }
