459d91f825de2b679cdbd1dfb26b92b7d74b547b
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2020-03-13 17:14:54 -0300

    styling profile page

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,7 +1,32 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 export default function Profile() {
-  return <div />;
+  const profile = useSelector(state => state.user.profile);
+
+  function handleSubmit() {}
+
+  return (
+    <Container>
+      <Form initialData={profile} onClick={handleSubmit}>
+        <Input name="name" placeholder="Nome completo" />
+        <Input name="email" type="email" placeholder="Seu endereço de e-mail" />
+
+        <hr />
+
+        <Input name="password" type="password" placeholder="Sua senha atual" />
+        <Input name="oldPassword" type="password" placeholder="Nova senha" />
+        <Input
+          name="newPassword"
+          type="password"
+          placeholder="Confirmação da senha"
+        />
+        <button type="submit">Atualizar perfil</button>
+      </Form>
+      <button type="button">Sair do GoBarber</button>
+    </Container>
+  );
 }
