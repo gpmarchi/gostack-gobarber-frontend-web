28a6ca3be602353514370712f0b48107f95c1096
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2020-03-13 18:11:38 -0300

    update provider profile

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,26 +1,55 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
+
+import { updateProfileRequest } from '../../store/modules/user/actions';
 
 import { Container } from './styles';
 
+const schema = Yup.object().shape({
+  name: Yup.string().required('O nome é obrigatório'),
+  email: Yup.string()
+    .email('Insira um e-mail válido')
+    .required('O e-mail é obrigatório'),
+  oldPassword: Yup.string().when('password', (password, field) =>
+    password ? field.required('Informe a senha antiga') : field
+  ),
+  password: Yup.string(),
+  confirmPassword: Yup.string().when('password', (password, field) =>
+    password
+      ? field
+          .required()
+          .min(6, 'No mínimo 6 caracteres')
+          .oneOf([Yup.ref('password')], 'A nova senha não confere')
+      : field
+  ),
+});
+
 export default function Profile() {
+  const dispatch = useDispatch();
   const profile = useSelector(state => state.user.profile);
 
-  function handleSubmit() {}
+  function handleSubmit(data) {
+    dispatch(updateProfileRequest(data));
+  }
 
   return (
     <Container>
-      <Form initialData={profile} onClick={handleSubmit}>
+      <Form schema={schema} initialData={profile} onSubmit={handleSubmit}>
         <Input name="name" placeholder="Nome completo" />
         <Input name="email" type="email" placeholder="Seu endereço de e-mail" />
 
         <hr />
 
-        <Input name="password" type="password" placeholder="Sua senha atual" />
-        <Input name="oldPassword" type="password" placeholder="Nova senha" />
         <Input
-          name="newPassword"
+          name="oldPassword"
+          type="password"
+          placeholder="Sua senha atual"
+        />
+        <Input name="password" type="password" placeholder="Nova senha" />
+        <Input
+          name="confirmPassword"
           type="password"
           placeholder="Confirmação da senha"
         />
